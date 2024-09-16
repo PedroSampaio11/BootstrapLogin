61275d3da6998ca904fcf83ca4f07c39007d3be1
PedroSampaioDev <pedrosampaio11@icloud.com>
2024-09-16 15:22:35 -0300

    script update

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const themeSelector = document.querySelector("#theme-selector");
+document.addEventListener("DOMContentLoaded", ()=>{
+    const selectorTheme = document.querySelector("#theme-selector")
 
-    themeSelector.addEventListener("change", (event) => {
-        const selectedValue = event.target.value;
-        if (selectedValue === "dark") {
-            document.documentElement.setAttribute("data-bs-theme", "dark");
-        } else if (selectedValue === "light") {
-            document.documentElement.setAttribute("data-bs-theme", "light");
-        } else {
-            document.documentElement.removeAttribute("data-bs-theme");
+    selectorTheme.addEventListener("change", (e)=>{
+        const theme = e.target.value
+        if(theme === "dark"){
+            document.documentElement.setAttribute("data-bs-theme", "dark")
+        }else if(theme === "light"){
+            document.documentElement.setAttribute("data-bs-theme", "light")
+        }else{
+            document.documentElement.removeAttribute("data-bs-theme")
         }
-    });
-});
-
+    })
+})
